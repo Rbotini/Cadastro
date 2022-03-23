76bb4186190ca68c0b104e266ff094378b5ead17
Renato Botini <botinirenato@gmail.com>
2022-03-22 21:50:32 -0300

    Criado opções do menu

--- Cliente/Program.cs
+++ Cliente/Program.cs
@@ -8,8 +8,12 @@ namespace Cliente
 {
     internal class Program
     {
+        enum Menu { listagem = 1,adicionar = 2, excluir = 3 , sair = 4};
         static void Main(string[] args)
         {
+            Console.WriteLine("Cadastro de Clientes");
+            Console.WriteLine("1-Listar\n2-Adicionar\n3-Excluir\n4-Sair");
+            Console.ReadLine();
         }
     }
 }
