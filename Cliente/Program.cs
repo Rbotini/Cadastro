b278e7a872fb7e1f1398a381a7eee86400e00579
Renato Botini <botinirenato@gmail.com>
2022-03-22 22:22:00 -0300

    Criado o metodo Adicionar

--- Cliente/Program.cs
+++ Cliente/Program.cs
@@ -16,6 +16,19 @@ namespace Cliente
                 get { return name; }
                 set { name = value; }
             }
+            private string email;
+
+            public string Email
+            {
+                get { return email; }
+                set { email = value; }
+            }
+            private string cpf;
+            public string Cpf
+            {
+                get { return cpf; }
+                set { cpf = value; }
+            }
         }
         enum Menu { listagem = 1,adicionar = 2, excluir = 3 , sair = 4};
         static void Main(string[] args)
@@ -29,17 +42,25 @@ namespace Cliente
                 case Menu.listagem:
                     break;
                     case Menu.adicionar:
-                    Cliente cliente = new Cliente();
-                    Console.WriteLine("Nome do Cliente:");
-                    cliente.Name = (Console.ReadLine());
-                    Console.WriteLine(cliente.Name);
-                    Console.ReadLine();
+                    Adicionar();
                     break;
                 case Menu.excluir:
                     break;
                 case Menu.sair:
                     break;
             }
+        }
+        static void Adicionar()
+        {
+            Cliente cliente = new Cliente();
+            Console.WriteLine("Nome do Cliente:");
+            cliente.Name = (Console.ReadLine());
+            Console.WriteLine("Insira o E-mail::");
+            cliente.Email = (Console.ReadLine());
+            Console.WriteLine("Insira o Cpf::");
+            cliente.Cpf = (Console.ReadLine());
+            Console.WriteLine(cliente.Name+cliente.Email);
+            Console.ReadLine();
         }
     }
 }
